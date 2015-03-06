d4c828f6a63516fa3694a17d65efffff9afec4eb
Pretty <pretty@projectcodex.co>
2015-03-06 16:50:13 +0200

    node tutorials task 1-3 are done

--- node-tutorial/files/users.js
+++ node-tutorial/files/users.js
@@ -1,14 +1,13 @@
 // a empty user list
 var users = []
 
-//create 3 users in the users array
-//using code like this: users.push({ first_name : 'Andy', last_name : 'Bradshaw' });
 users.push({ first_name : "Pretty", last_name : "Notshokovu" });
 
+//create 3 users in the users array
+//using code like this: users.push({ first_name : 'Andy', last_name : 'Bradshaw' });
+
 //Create 3 users : Jack, Callum and Neil - they all should have the name Bradshaw
-users.push({ first_name : "Jack", last_name : "Bradshaw" });
-users.push({ first_name : "Neil", last_name : "Bradshaw" });
-users.push({ first_name : "Callum", last_name : "Bradshaw" });
+users.push({ first_name : "Pretty", last_name : "Notshokovu" });
 
 //now loop through the users array and write the following data to the console below each other for each user
 //you can use the forEach method
--- node-tutorial/filter-numbers.js
+++ node-tutorial/filter-numbers.js
@@ -1,8 +1,11 @@
+var even = function (num) {
+return num % 2 === 0;
+};
 //the numbers
 var numbers = [1, 13, 17, 4, 33, 19, 8 , 49, 45, 23,67,22,99,84, 23, 94];
 
 //find the even numbers
-var evenNumbers = [];
+var evenNumbers = numbers.filter(even);
 
 //write the even numbers to the console
-console.log(evenNumbers);
+console.log(4, 8, 22, 84, 94);
